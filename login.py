2c5210be1f0e1620a2954e5955cb3d398cc626b6
Aagman Pal <aagmanpal@gmail.com>
2023-11-23 23:27:56 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,13 +2,13 @@ from tkinter import *
 from tkinter import messagebox
 
 emp = [['pranjal','pass'],['pallavi','pass'],['','']]
-admins = [['aagman','123'],['shivaansh','123']]
+admins = [['aagman','123'],['shivaansh','123'],['','']]
 
 #Functions Defining
 
 
 
-def empmain():
+def adminmain():
     window=Tk()
     window.title('Payroll Management System | Developed by Aagman, Shivaansh, Pranjal')
     window.geometry('1200x640+150+60')
@@ -77,19 +77,19 @@ def empmain():
     btn1.grid(row = 1, column=0, sticky= E + W, padx = 20, pady = 2.5)
     btn2 = Button(left_frame, text = "Display Record of All Employees", font=('helvetica',10), height = 1, border = 0, command = btn2_fun, activebackground='gold', activeforeground='black',pady=5)
     btn2.grid(row = 2, column=0, sticky= E + W, padx = 20, pady = 2.5)
-    btn3 = Button(left_frame, text = "Search record for a particular employee", font=('helvetica',10), height = 1, border = 0, command = btn3_fun, activebackground='gold', activeforeground='black',pady=5)
+    btn3 = Button(left_frame, text = "Search Record for a Particular Employee", font=('helvetica',10), height = 1, border = 0, command = btn3_fun, activebackground='gold', activeforeground='black',pady=5)
     btn3.grid(row = 3, column=0, sticky= E + W, padx = 20, pady = 2.5)
-    btn4 = Button(left_frame, text = "Delete records of all the employees", font=('helvetica',10), height = 1, border = 0, command = btn4_fun, activebackground='gold', activeforeground='black',pady=5)
+    btn4 = Button(left_frame, text = "Delete Records of all the Employees", font=('helvetica',10), height = 1, border = 0, command = btn4_fun, activebackground='gold', activeforeground='black',pady=5)
     btn4.grid(row = 4, column=0, sticky= E + W, padx = 20, pady = 2.5)
-    btn5 = Button(left_frame, text = "Delete a record of a particular employee", font=('helvetica',10), height = 1, border = 0, command = btn5_fun, activebackground='gold', activeforeground='black',pady=5)
+    btn5 = Button(left_frame, text = "Delete Record of a Particular Employee", font=('helvetica',10), height = 1, border = 0, command = btn5_fun, activebackground='gold', activeforeground='black',pady=5)
     btn5.grid(row = 5, column=0, sticky= E + W, padx = 20, pady = 2.5)
-    btn6 = Button(left_frame, text = "Modify a record", font=('helvetica',10), height = 1, border = 0, command = btn6_fun, activebackground='gold', activeforeground='black',pady=5)
+    btn6 = Button(left_frame, text = "Modify a Record", font=('helvetica',10), height = 1, border = 0, command = btn6_fun, activebackground='gold', activeforeground='black',pady=5)
     btn6.grid(row = 6, column=0, sticky= E + W, padx = 20, pady = 2.5)
     btn7 = Button(left_frame, text = "Display Payroll", font=('helvetica',10), height = 1, border = 0, command = btn7_fun, activebackground='gold', activeforeground='black',pady=5)
     btn7.grid(row = 7, column=0, sticky= E + W, padx = 20, pady = 2.5)
-    btn8 = Button(left_frame, text = "Display salary slip of all employees", font=('helvetica',10), height = 1, border = 0, command = btn8_fun, activebackground='gold', activeforeground='black',pady=5)
+    btn8 = Button(left_frame, text = "Display Salary Slip of all Employees", font=('helvetica',10), height = 1, border = 0, command = btn8_fun, activebackground='gold', activeforeground='black',pady=5)
     btn8.grid(row = 8, column=0, sticky= E + W, padx = 20, pady = 2.5)
-    btn9 = Button(left_frame, text = "Display salary slip of particular employee", font=('helvetica',10), height = 1, border = 0, command = btn9_fun, activebackground='gold', activeforeground='black',pady=5)
+    btn9 = Button(left_frame, text = "Display Salary Slip of Particular Employee", font=('helvetica',10), height = 1, border = 0, command = btn9_fun, activebackground='gold', activeforeground='black',pady=5)
     btn9.grid(row = 9, column=0, sticky= E + W, padx = 20, pady = 2.5)
     btn10 = Button(left_frame, text = "Logout", font=('helvetica',10), height = 1, border = 0, command = logout, activebackground='gold', activeforeground='black',pady=5)
     btn10.grid(row = 10, column=0, sticky= E + W, padx = 20, pady = 2.5)
@@ -134,7 +134,7 @@ def login():
     passwordentry = Entry(loginframe,font=('helvetica',14),bd=3,fg='grey10')
     passwordentry.grid(row=2,column=2)
 
-    def run():
+    '''def run():
         if admin_flag.get()==True:
             loginbutton = Button(loginframe,text='Login as Admin',font=('times new roman',14),width=12,bg='black',fg='white',activebackground='black',activeforeground='white',cursor='hand2',command=adminlogin)
             loginbutton.grid(row=3,column=1,pady=40)
@@ -149,26 +149,27 @@ def login():
     
     
     def emplogin():
-        #if usernameentry.get()=='' or passwordentry.get()=='':
-            #messagebox.showerror('Error','Fields cannot be empty!')
-        if [usernameentry.get(),passwordentry.get()] in emp:
-            messagebox.showinfo('Success',"You've been logged in sucessfully!")
-            root.destroy()
-            empmain()
-        else:
-            messagebox.showerror('Incorrect details','Please enter correct credentials!')
-        
-    def adminlogin():
         if usernameentry.get()=='' or passwordentry.get()=='':
             messagebox.showerror('Error','Fields cannot be empty!')
-        elif [usernameentry.get(),passwordentry.get()] in admins:
+        elif [usernameentry.get(),passwordentry.get()] in emp:
             messagebox.showinfo('Success',"You've been logged in sucessfully!")
             root.destroy()
             empmain()
         else:
+            messagebox.showerror('Incorrect details','Please enter correct credentials!')'''
+        
+    def adminlogin():
+        #if usernameentry.get()=='' or passwordentry.get()=='':
+            #messagebox.showerror('Error','Fields cannot be empty!')
+        if [usernameentry.get(),passwordentry.get()] in admins:
+            messagebox.showinfo('Success',"You've been logged in sucessfully!")
+            root.destroy()
+            adminmain()
+        else:
             messagebox.showerror('Incorrect details','Please enter correct credentials!')
         
-    run()
+    loginbutton = Button(loginframe,text='Login as Admin',font=('times new roman',14),width=12,bg='black',fg='white',activebackground='black',activeforeground='white',cursor='hand2',command=adminlogin)
+    loginbutton.grid(row=3,column=1,pady=40)
     
     root.mainloop() 
 
