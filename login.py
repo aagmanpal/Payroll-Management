a646e9a7a90760673229d3ac5654d7ff476ce2a5
Aagman Pal <aagmanpal@gmail.com>
2023-11-25 18:06:56 +0530

    Update login.py

--- login.py
+++ login.py
@@ -15,7 +15,7 @@ def adminmain():
     window.minsize(1200,640)
     window.resizable(False,False)
     window.iconbitmap("salary.ico")
-    
+    window.configure(bg='black')
     #===========Header Frame===========
     header_frame = Frame(window,bg='black', height=60, width = 1280)
     header_frame.place(x=0,y=0)
@@ -40,8 +40,82 @@ def adminmain():
     left_frame.place(x=0,y=60,width=350,height=558)
     
     #Defining btn functions
-    def btn1_fun():
-        pass
+    def addemp():
+        rightframe.destroy()
+        addemp_frame = Frame(window,bg='white')
+        addemp_frame.columnconfigure(0, weight = 1)
+        addemp_frame.columnconfigure(1, weight = 1)
+        addemp_frame.columnconfigure(2, weight = 1)
+        addemp_frame.columnconfigure(3, weight = 1)
+        addemp_frame.rowconfigure(0, weight = 1)
+        addemp_frame.rowconfigure(1, weight = 1)
+        addemp_frame.rowconfigure(2, weight = 1)
+        addemp_frame.rowconfigure(3, weight = 1)
+        addemp_frame.rowconfigure(4, weight = 1)
+        addemp_frame.rowconfigure(5, weight = 1)
+        addemp_frame.rowconfigure(6, weight = 1)
+        addemp_frame.place(x=350,y=110,width=840,height=508)
+        #==========================Employee Details Variable========================================================
+        emp_id = StringVar()
+        emp_name = StringVar()
+        emp_desig = StringVar()
+        emp_age = StringVar()
+        emp_gender = StringVar()
+        emp_email = StringVar()
+        emp_dob = StringVar()
+        emp_doj = StringVar()
+        emp_accno = StringVar()
+        emp_contact = StringVar()
+        emp_add = StringVar()
+        #================== Details of Employess to Add==============================================================
+        text1 = Label(addemp_frame,text='Employee ID:',font=('times new roman',16),bg='white')
+        text1.grid(column=0,row=0)
+        text2 = Label(addemp_frame,text='Employee Name:',font=('times new roman',16),bg='white')
+        text2.grid(column=0,row=1)
+        text3 = Label(addemp_frame,text='Designation:',font=('times new roman',16),bg='white')
+        text3.grid(column=0,row=2)
+        text4 = Label(addemp_frame,text='Age:',font=('times new roman',16),bg='white')
+        text4.grid(column=0,row=3)
+        text5 = Label(addemp_frame,text='Gender:',font=('times new roman',16),bg='white')
+        text5.grid(column=0,row=4)
+        text6 = Label(addemp_frame,text='Email:',font=('times new roman',16),bg='white')
+        text6.grid(column=2,row=0)
+        text7 = Label(addemp_frame,text='D.O.B:',font=('times new roman',16),bg='white')
+        text7.grid(column=2,row=1)
+        text8 = Label(addemp_frame,text='D.O.J:',font=('times new roman',16),bg='white')
+        text8.grid(column=2,row=2)
+        text9 = Label(addemp_frame,text='Acc No.:',font=('times new roman',16),bg='white')
+        text9.grid(column=2,row=3)
+        text10 = Label(addemp_frame,text='Contact No.:',font=('times new roman',16),bg='white')
+        text10.grid(column=2,row=4)
+        text11 = Label(addemp_frame,text='Address:',font=('times new roman',16),bg='white')
+        text11.grid(column=0,row=5)
+        #==============================Respective Entry Fields=======================================================
+        entry1 = Entry(addemp_frame, textvariable=emp_id ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry1.grid(column=1,row=0)
+        entry2 = Entry(addemp_frame, textvariable=emp_name ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry2.grid(column=1,row=1)
+        entry3 = Entry(addemp_frame, textvariable=emp_desig ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry3.grid(column=1,row=2)
+        entry4 = Entry(addemp_frame, textvariable=emp_age ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry4.grid(column=1,row=3)
+        entry5 = Entry(addemp_frame, textvariable=emp_gender ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry5.grid(column=1,row=4)
+        entry6 = Entry(addemp_frame, textvariable=emp_email ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry6.grid(column=3,row=0)
+        entry7 = Entry(addemp_frame, textvariable=emp_dob ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry7.grid(column=3,row=1)
+        entry8 = Entry(addemp_frame, textvariable=emp_doj ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry8.grid(column=3,row=2)
+        entry9 = Entry(addemp_frame, textvariable=emp_accno ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry9.grid(column=3,row=3)
+        entry10 = Entry(addemp_frame, textvariable=emp_contact ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry10.grid(column=3,row=4)
+        entry11 = Entry(addemp_frame, textvariable=emp_add ,bg='lightyellow',bd=3,font=('Times new roman',16))
+        entry11.grid(column=1,row=5,columnspan=3,sticky= E + W,padx=10)
+        #================================Buttons in Add Employee========================================================
+        button1 = Button(addemp_frame,text='Add Employee',bg='#89CFF0',fg='black',font=('helvetica',14),bd=3,relief=SOLID,cursor='hand2',activebackground='black',activeforeground='white')
+        button1.grid(row=6,column=0,columnspan=4)
 
     def btn2_fun():
         pass
@@ -74,7 +148,10 @@ def adminmain():
     
     leftframe_text = Label(left_frame, text = "Welcome Employee!", font=('Comic Sans MS',20), height = 1, border = 3,background='burlywood1',relief=RIDGE)
     leftframe_text.grid(row = 0, column=0, sticky= E + W, padx = 5, pady = 2.5)
-    btn1 = Button(left_frame, text = "Add Employee Record", font=('helvetica',10), height = 1, border = 0, command = btn1_fun, activebackground='gold', activeforeground='black',pady=5)
+    
+    #======================Menu Buttons======================================================
+    
+    btn1 = Button(left_frame, text = "Add Employee Record", font=('helvetica',10), height = 1, border = 0, command = addemp, activebackground='gold', activeforeground='black',pady=5)
     btn1.grid(row = 1, column=0, sticky= E + W, padx = 20, pady = 2.5)
     btn2 = Button(left_frame, text = "Display Record of All Employees", font=('helvetica',10), height = 1, border = 0, command = btn2_fun, activebackground='gold', activeforeground='black',pady=5)
     btn2.grid(row = 2, column=0, sticky= E + W, padx = 20, pady = 2.5)
@@ -94,9 +171,13 @@ def adminmain():
     btn9.grid(row = 9, column=0, sticky= E + W, padx = 20, pady = 2.5)
     btn10 = Button(left_frame, text = "Logout", font=('helvetica',10), height = 1, border = 0, command = logout, activebackground='gold', activeforeground='black',pady=5)
     btn10.grid(row = 10, column=0, sticky= E + W, padx = 20, pady = 2.5)
-    #==========Right Frame=============
     
-    #==========Footer Frame============
+    #=============================================Right Frame==============================================================
+    rightframe = Frame(window,bg='yellow')
+    rightframe.place(x=350,y=110,width=840,height=508)
+    rightframe_header = Frame(window, border = 2,background='burlywood1',relief=SOLID)
+    rightframe_header.place(x=350,y=60,width=840,height=50)
+    #=============================================Footer Frame=============================================================
     footer_frame = Frame(window,bg='black',width=1200,height=25)
     footer_frame.place(x=0,y=615)
     footer_text=Label(footer_frame,text='Developed by Aagman,Shivaansh, and Pranjal--Class 12th A',font=('helvetica',10,'bold'),fg='white',bg='black')
